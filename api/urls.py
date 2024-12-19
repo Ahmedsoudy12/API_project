44e95013c954d80b1352f781401320311e710662
Ahmed Soudy <ahmed.soudy30@yahoo.com>
2024-12-19 14:04:28 +0200

    last-work-in-nagwa-on-api-project

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_users, create_user
+from .views import get_users, create_user, user_detail
 
 
 urlpatterns = [
     path('users/',get_users,name="get_users"),    # This is the route that i use to make an API request
                                        # get_user is the function that we are calling when we are making the request
     path('users/create',create_user,name="create_user"),
+    path('users/<int:pk>',user_detail,name="user_detail"),  # 34an a input el id bta3 el user elly 3ayz a3mello edit
 ]
--- api/views.py
+++ api/views.py
@@ -12,10 +12,8 @@ from .serializer import UserSerializer
 @api_view(['GET'])   # Here i am defining the get_user to be a GET request with the decorator api_view
 def get_users(request):    # End point function
     users = User.objects.all()
-    serilizer = UserSerializer(users)
-    return Response(UserSerializer({'name':"Ahmed",'age':25}).data)    # returning a Response object and inside it the serialized data
-
-
+    serilizer = UserSerializer(users, many=True)
+    return Response(serilizer.data)    # returning a Response object and inside it the serialized data
 
 
 @api_view(['POST'])   # Here i am defining the create_user to be a POST request with the decorator api_view
@@ -26,6 +24,29 @@ def create_user(request):    #
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
  
+ 
+@api_view(['GET','PUT','DELETE'])
+def  user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 
 
